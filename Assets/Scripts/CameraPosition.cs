8886500b37db529274d9cd68a4f59743fd15bd5c
Ding <mlin702651@gmail.com>
2021-08-18 21:37:34 +0800

    add ice shader

---
+++ Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    [SerializeField]
+    GameObject mainCamera;
+
+    public Vector3 moveTo;
+    [SerializeField]
+    bool isMoving=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMoving){
+            mainCamera.transform.position+=moveTo;
+            isMoving=false;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        if(other.tag=="nextLevel"){
+            isMoving=true;   
+        }
+    }
+}
